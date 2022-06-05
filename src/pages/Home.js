ba3c5a6a8f2be327e9680dcaa7320466f71f7edc
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-06-06 02:59:06 +0700

    coolnote: create components

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NoteCard from '../components/NoteCard'
+
+const Home = () => {
+    return (
+        <div>
+            <NoteCard />
+        </div>
+    )
+}
+
+export default Home
