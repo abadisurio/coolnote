83bd0a01e0780a580380c989446569b9bed4ea97
Abadi Suryo <abadisuryosetiyo@gmail.com>
2022-06-07 02:29:43 +0700

    coolnote: add features

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getInitialData } from '../utils'
+import { SearchIcon } from '@heroicons/react/solid'
 
 const initialData = [{
     id: 0,
@@ -19,7 +20,9 @@ const initialNewNote = {
 const CardGrid = () => {
 
     const [data, setData] = useState(initialData)
+    const [searchKey, setSearchKey] = useState("")
     const [newNote, setNewNote] = useState(initialNewNote)
+    const [searchedNote, setSearchedNote] = useState([])
 
     const [isLoading, setLoading] = useState(true)
 
@@ -28,6 +31,16 @@ const CardGrid = () => {
         setData(fetchedData)
         setLoading(false)
     }, [])
+
+    const handleSearch = (e) => {
+        // console.log('e', e)
+        const newSearchKey = e.target.value
+        setSearchKey(newSearchKey)
+        setSearchedNote(
+            data.filter(item => item.title.toLowerCase().includes(newSearchKey.toLowerCase()))
+        )
+        console.log('data', data)
+    }
 
     const handleChange = (e) => {
         setNewNote(prevState => ({
@@ -51,19 +64,25 @@ const CardGrid = () => {
         <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4'>
             {isLoading && <h1>Loading</h1>}
             <div className='flex flex-col'>
+                <div className='rounded-xl border border-white m-2 p-3 hover:bg-slate-700 transition duration-150 flex flex-row content-center'>
+                    <SearchIcon class="h-5 w-5 self-center mr-3" />
+                    <input className='flex bg-transparent w-full text-xl text-sm' name='search' value={searchKey} onChange={handleSearch} onFocus={(e) => e.target.select()} placeholder="Search" />
+                </div>
                 <div className='rounded-xl border border-white m-2 p-3 hover:bg-slate-700 transition duration-150 flex-1 flex flex-col'>
-                    <input className='bg-transparent w-full text-xl' name='title' value={newNote.title} onChange={handleChange} />
+                    <input className='bg-transparent w-full text-xl mb-3' name='title' value={newNote.title} onChange={handleChange} />
                     <textarea className='bg-transparent w-full flex-1 mb-3' name='body' value={newNote.body} onChange={handleChange} rows={3} />
                     <button className='bg-green-500/80 px-2 py-1 rounded-lg ' onClick={saveNote}>Save</button>
 
                 </div>
             </div>
-            {data.map(item => {
+            {(searchKey === "" ? data : searchedNote).map(item => {
                 // console.log(item)
                 return (
-                    <div key={item.id} className='rounded-xl border border-white m-2 p-3 hover:bg-slate-700 transition duration-150'>
-                        <h1 className='text-xl'>{item.title}</h1>
-                        {item.body}
+                    <div key={item.id} className='rounded-xl border border-white m-2 p-3 hover:bg-slate-700 transition duration-150 h-64'>
+                        <h1 className='text-xl mb-3'>{item.title}</h1>
+                        <p className='line-clamp-3'>
+                            {item.body}
+                        </p>
                     </div>
                 )
             })}
